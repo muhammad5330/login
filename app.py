4757a1aa44778e0c1346a2d45dd5134cf23ac649
muhammad5330 <moto14664@gmail.com.>
2024-06-26 17:23:37 +0500

    ok

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import mysql.connector
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
